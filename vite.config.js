4d1bf69667b7dd1d4c0675329c389c375070cf71
GoatDRyan <mumbata.ryan2@gmail.com>
2025-11-12 11:48:11 +0100

    maj

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio/',
+  base: '/portfolio/', // ⚠️ Mets ici le nom EXACT de ton repo GitHub
+  build: {
+    outDir: 'dist',
+  },
   server: {
+    // Pour le mode dev
+    historyApiFallback: true,
+  },
+  preview: {
+    // Pour la version buildée (utile pour tester avant déploiement)
     historyApiFallback: true,
   },
 })
