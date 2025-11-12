b2f00761eeb6da92c87b830e86d08b518d1cc190
GoatDRyan <mumbata.ryan2@gmail.com>
2025-11-12 13:51:06 +0100

    encore

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/portfolio/',
   build: {
     outDir: 'dist',
-  },
-  server: {
-    // Pour le mode dev
-    historyApiFallback: true,
-  },
-  preview: {
-    // Pour la version buildée (utile pour tester avant déploiement)
-    historyApiFallback: true,
   },
 })
